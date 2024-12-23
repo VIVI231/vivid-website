82a90379db4f4e0c9ae00091ce6c8721c26da054
VIVI231 <viv1d.business13@gmail.com>
2024-12-24 06:00:03 +0700

    CROT FUTURE

--- src/ThickMommy.js
+++ src/ThickMommy.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const ThickMommy = () => {
+  const notify = () => {
+    setCount(count + 1)
+    if (count <= 5){
+      toast("AHH");
+      
+    }
+    else {
+      toast("AKU CROT BANG AHAHAHAHAH");
+      setthick("/absurd.jpg")
+    }
+  };
+  const [count, setCount] = useState(0);
+  const [thick, setthick] = useState("/thickmommy.jpg");
   return (
     <div>
       <h4>HIDUP SEPERTI LARRY</h4>
       <i>Hidup seperti larry</i>
-      <img src='/thickmommy.jpg' className='thickmommyuwu'></img>
+      {count}
+      <button onClick={notify}>TUSUK</button>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      <img src={thick} className="thickmommyuwu"></img>
     </div>
-  )
-}
+  );
+};
 
-export default ThickMommy
+export default ThickMommy;
